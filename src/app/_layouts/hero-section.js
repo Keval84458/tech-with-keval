22d26d2066957b8a8de0208923cfda10806b88db
Keval84458 <kevalmakvana84458@gmail.com>
2025-01-08 09:30:14 +0530

    km-hero-changes

--- src/app/_layouts/hero-section.js
+++ src/app/_layouts/hero-section.js
@@ -10,53 +10,57 @@ const HeroSection = () => {
         bgImage="/assets/background/site-bg.jpg"
         bgSize="cover"
         bgRepeat="no-repeat"
-        height="560px"
         w="100%"
+        overflowY="hidden"
       >
-        <Text
-          color="white.100"
-          pt="3rem"
-          pl="2rem"
-          fontSize="650"
-          fontWeight="bold"
-          textAlign="center"
-          bgGradient="linear-gradient(180deg, rgba(2,0,36,1) 0%, rgb(120, 225, 246) 9%, rgb(255, 255, 255) 100%)"
-          bgClip="text"
+        <Box
+          my={{ base: "10rem", md: 0 }}
+          py={{ md: "5rem" }}
+          transition="all .5s ease"
         >
-          Tech with Keval
-        </Text>
+          <Text
+            color="white.100"
+            fontSize={{ base: "300", md: "650" }}
+            fontWeight="bold"
+            textAlign="center"
+            bgGradient="linear-gradient(180deg, rgba(2,0,36,1) 0%, rgb(120, 225, 246) 9%, rgb(255, 255, 255) 100%)"
+            bgClip="text"
+          >
+            Tech with Keval
+          </Text>
 
-        <Text
-          color="white.100"
-          textAlign="center"
-          fontSize="300"
-          fontFamily="heading"
-          fontWeight="bold"
-        >
-          Learn about
-        </Text>
-        <Text
-          as="span"
-          textAlign="center"
-          fontSize="150"
-          color="primary.300"
-          fontWeight="bold"
-        >
-          <Typewriter
-            options={{
-              strings: [
-                "React Js",
-                "Next Js",
-                "Chakra UI",
-                "Pocketbase",
-                "Strapi",
-              ],
-              loop: true,
-              autoStart: true,
-              cursor: "_",
-            }}
-          />
-        </Text>
+          <Text
+            color="white.100"
+            textAlign="center"
+            fontSize={{ base: "175", md: "300" }}
+            fontFamily="heading"
+            fontWeight="bold"
+          >
+            Learn about
+          </Text>
+          <Text
+            as={"span"}
+            textAlign="center"
+            fontSize={{ base: "100", md: "150" }}
+            color="primary.300"
+            fontWeight="bold"
+          >
+            <Typewriter
+              options={{
+                strings: [
+                  "React Js",
+                  "Next Js",
+                  "Chakra UI",
+                  "Pocketbase",
+                  "Strapi",
+                ],
+                loop: true,
+                autoStart: true,
+                cursor: "_",
+              }}
+            />
+          </Text>
+        </Box>
       </Box>
     </>
   );
